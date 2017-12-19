176ec73293a2957ffefa56ec602a9b2df2af5b79
pedrodisatec <pedro.perez@disatec.es>
2017-12-19 12:57:23 +0100

    Commit 19-Dec-2017

--- src/app/main/seguridad/seguridad.component.ts
+++ src/app/main/seguridad/seguridad.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-seguridad',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeguridadComponent implements OnInit {
 
-  constructor() { }
+	public data: any[];
 
-  ngOnInit() {
+  constructor(private _http: Http) {}
+
+      ngOnInit(): void {
+	this._http.get("assets/data_contact.json")
+      .subscribe((data)=> {
+        setTimeout(()=> {
+          this.data = data.json();
+        }, 2000);
+      });
+	  
   }
+  
+  public ngForCallback() {
+		alert('ngFor finish');
+  }
+  
+
 
 }
