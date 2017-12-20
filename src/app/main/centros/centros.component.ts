7170dcd9d23e5904c103b2b2d410130b3c95625e
pedrodisatec <pedro.perez@disatec.es>
2017-12-20 09:50:00 +0100

    Commit 20-Dec-2017

--- src/app/main/centros/centros.component.ts
+++ src/app/main/centros/centros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Http} from "@angular/http";
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 
 @Component({
@@ -15,31 +16,43 @@ export class CentrosComponent implements OnInit {
   public sortBy = "id";
   public sortOrder = "asc";
   private showId = false;
+  
+  private GoToCenter = "";
 
   
   showTableCenter:boolean = true;
   showCenter:boolean = false;
  
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http, private actroute:ActivatedRoute, private router: Router) { }
 
     ngOnInit(): void {
-    this._http.get("assets/data_center.json")
-      .subscribe((data)=> {
-			this.data = data.json();
-			console.log(this.data);	
-			  /*var options = { 
-				fieldSeparator: ';',
-				quoteStrings: '"',
-				decimalseparator: '.',
-				showLabels: true, 
-				showTitle: false,
-				useBom: true
-			  };*/			
-			//new Angular2Csv(this.data, 'My Report', options);	
-			//Para generar el Csv llamaremos a una funcion que nos devuelva el json especifico. Con la llamada de más arriba se genera el CSV.
-      });
-	  
+	
+	this.actroute.queryParams
+    .subscribe(params => {
+        this.GoToCenter = params['param']; 
+    });
+	
+	if(this.GoToCenter == null){
+			this._http.get("assets/data_center.json")
+			  .subscribe((data)=> {
+					this.data = data.json();
+					console.log(this.data);	
+					  /*var options = { 
+						fieldSeparator: ';',
+						quoteStrings: '"',
+						decimalseparator: '.',
+						showLabels: true, 
+						showTitle: false,
+						useBom: true
+					  };*/			
+					//new Angular2Csv(this.data, 'My Report', options);	
+					//Para generar el Csv llamaremos a una funcion que nos devuelva el json especifico. Con la llamada de más arriba se genera el CSV.
+			  });
+	}
+	else{
+			this.showCenterFunction();
+	}
   }
   
   showCenterFunction(){
--- src/app/main/contactos/contactos.component.ts
+++ src/app/main/contactos/contactos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Http} from "@angular/http";
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-contactos',
@@ -18,7 +19,7 @@ export class ContactosComponent implements OnInit {
   showTableContacts:boolean = true;
   showContact:boolean = false;
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http, private router: Router) { }
 
     ngOnInit(): void {
     this._http.get("assets/data_contact.json")
@@ -30,6 +31,10 @@ export class ContactosComponent implements OnInit {
   showContactFunction(){
 	this.showContact = true;
 	this.showTableContacts = false;
+  }
+  
+  goCenter(id_center){
+		this.router.navigate(['/centros'], { queryParams: { param: 'gotocenter', id: id_center } });
   }
 
 }
